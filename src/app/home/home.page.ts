82bf7fc130327a3be3c0fdee63738d5d9d7c7fa1
BroxiAge <nahuel.di.fiore@gmail.com>
2023-01-16 14:33:03 -0300

    mostrar los archivos de Filesystem

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-import { Directory, Filesystem } from '@capacitor/filesystem'
-import { Platform } from '@ionic/angular';
+import { Directory, FileInfo, Filesystem } from '@capacitor/filesystem'
+import { LoadingController, Platform } from '@ionic/angular';
+import { async } from 'rxjs';
 const IMAGE_DIR = 'stored-images';
 
 interface LocalFile {
@@ -16,19 +17,64 @@ interface LocalFile {
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  
+  images: LocalFile[] = [];
 
-  constructor( private platform: Platform ) {}
+  constructor( private platform: Platform, private loadingCtrl: LoadingController ) {}
 
   async ngOnInit() {
-
+    this.loadFiles()
   }
+
+  async loadFiles() {
+    this.images = [];
+
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading data...'
+    });
+    await loading.present();
+
+    Filesystem.readdir({
+      directory: Directory.Data,
+      path: IMAGE_DIR
+    }).then(result => {
+      console.log('HERE', result)
+      this.loadFileData(result.files);
+    }, async err => {
+      // console.log('error', err)
+      await Filesystem.mkdir({
+        directory: Directory.Data,
+        path: IMAGE_DIR
+      });
+    }).then(_ =>{
+      loading.dismiss();
+    })
+  }
+
+
+// Get the actual base64 data of an image
+// base on the name of the file
+async loadFileData(fileNames: FileInfo[]) {
+	for (let f of fileNames) {
+  const filePath = `${IMAGE_DIR}/${f.name}`
+  const readFile = await Filesystem.readFile({
+    directory: Directory.Data,
+    path: filePath
+  })
+  this.images.push({
+    name: f.name,
+    path: filePath,
+    data: `data:image/jpeg;base64,${readFile.data}`
+  })
+	}
+}
 
   async selectImage() {
     const image = await Camera.getPhoto({
       quality: 90,
       allowEditing: false,
       resultType: CameraResultType.Uri,
-      source: CameraSource.Photos
+      source: CameraSource.Camera
     });
     console.log(image);
     
@@ -51,6 +97,7 @@ export class HomePage implements OnInit {
     });
 
     console.log('saved:', savedFile);
+    this.loadFiles();
     
 
   }
@@ -82,5 +129,13 @@ export class HomePage implements OnInit {
     };
     reader.readAsDataURL(blob);
   });
+
+  startUpload() {
+
+  }
+
+  deleteImage() {
+    
+  }
 
 }
